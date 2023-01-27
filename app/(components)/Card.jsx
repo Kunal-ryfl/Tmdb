f3882f79c5e76e969e81bae2d97a60f02c02e97b
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-01-27 18:24:10 +0530

    g

--- app/(components)/Card.jsx
+++ app/(components)/Card.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Card = ({movie:{title,poster_path,id,release_date}}) => {
   return (
-    <Link href={`/${id}`}>
+    <Link href={`/movies/${id}`}>
     <div className='card'> 
         <img src={` https://image.tmdb.org/t/p/original${poster_path}`} />
         <h1 className='h-small'>{title}</h1>
