21a9ee6c5022f680b4ca6a939cb0e3273f3fcf4a
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-02-20 17:53:42 +0530

    g

--- app/(components)/Card.jsx
+++ app/(components)/Card.jsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
@@ -11,32 +11,20 @@ const myLoader = ({src}) => {
 }
 
 const Card = ({movie:{title,poster_path,id,release_date}}) => {
-  const [isLoaded, setIsLoaded] = useState(false)
-  // const [skel, setSkel] = useState(true)
-  
-  const handle = ()=>{
-       setIsLoaded(true)
-      //  setSkel(false)  
-  }
-  
   
   return (
     
     <Link href={`/movies/${id}`} >
 
     <div className='card overflow-hidden'> 
-    {!isLoaded  && <Skeleton />}
-
-        {
-          poster_path    &&
+          {
+          poster_path  &&
            <Image
           loader={myLoader}
           src={poster_path}
           alt="img"
           height={100}
           width={1000}         
-          onLoadingComplete={() => handle() }
-          // onLoad={()=>setSkel(false)}
           className='transition duration-5 ease-in-out rounded hover:scale-105'
 />
 }
