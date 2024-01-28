bebad91a0d7fa13e2c8ba1054486f0ca8e708d94
Kunal-ryfl <kunalsharma070806@gmail.com>
2024-01-28 23:41:38 +0530

    media

--- app/(components)/Hero.jsx
+++ app/(components)/Hero.jsx
@@ -3,85 +3,92 @@ import React, { useState, useEffect, Suspense } from "react";
 import Card from "./Card";
 import InlineCard from "./InlineCard";
 import { useStateContext } from "../../context/StateContext";
-import Skeleton from './Skeleton'
-import {use} from "react"
+import Skeleton from "./Skeleton";
+import { use } from "react";
 import Image from "next/image";
-import {BsLink45Deg} from 'react-icons/bs'
+import { BsLink45Deg } from "react-icons/bs";
 import Link from "next/link";
 import InSkel from "./InSkel";
 import Trending from "./Trending";
 import Upcoming from "./Upcoming";
 
-
-
 async function getPopular() {
-  const res =  await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API}`,{ cache: 'no-store' } );
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API}`,
+    { cache: "no-store" }
+  );
   return res.json();
 }
 
-function get_random (list) {
-  return list[Math.floor((Math.random()*list.length))];
+function get_random(list) {
+  return list[Math.floor(Math.random() * list.length)];
 }
 
+let a = getPopular();
 
-let a = getPopular()
-
-export default  function Main() {
+export default function Main() {
   // const { popular, search } = useStateContext();
   // const fetch =  await getPopular()
- const popular = use(a) 
-//  console.log("p = ",popular)
+  const popular = use(a);
+  //  console.log("p = ",popular)
 
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
   if (!mounted) {
-    return <InSkel/>
+    return <InSkel />;
   }
 
   const random = get_random(popular.results);
   return (
-    < div className="   " >
-    
-    <div className="   relative px-3 grid  pt-[400px]">
-      <div  className=" -z-10  w-screen absolute  top-0 h-screen">
+    <div className="   ">
+      <div className="   relative px-3 grid  pt-[400px]">
+        <div className=" -z-10  w-screen absolute  top-0 h-screen">
+          <Image
+            src={`https://image.tmdb.org/t/p/original${random.backdrop_path}`}
+            fill
+            alt=""
+            priority
+            className="    object-cover -z-10  "
+          />
 
-
-  <Image src={`https://image.tmdb.org/t/p/original${random.backdrop_path}`} fill alt="" priority className="    object-cover -z-10  "/>
-
-      <div className=" opacity-100 absolute top-0  bg-gradient-to-b  from-black/50 from-20% via-black via-80% to-neutral-900 to-100%  h-screen  w-screen" ></div>
-      
-      </div>
-
-
-      <div className=" px-4 md:px-10 py-2 absolute max-w-[330px] md:max-w-[800px] top-20 md:top-32  ">
-        <h1 className=" text-[24px] mb-2  md:text-3xl font-bold">{random.title}</h1> 
-        <div className="  font-semibold text-sm flex " >
-
-        <p className="  text-green-600 mr-3"> {Math.round( random.vote_average*10)}% votes</p>
-        <p>{random.release_date}</p>
-        
+          <div className=" opacity-100 absolute top-0  bg-gradient-to-b  from-black/50 from-20% via-black via-80% to-neutral-900 to-100%  h-screen  w-screen"></div>
         </div>
-        <p className=" my-4 line-clamp-4 text-sm text-gray-300 md:text-base">{random.overview}</p>
-       <Link href={`movies/${random.id}`}>
-        <button className="  bg-white flex items-center rounded px-2 py-1 font-bold text-black"><BsLink45Deg  className="  "/> Open</button>
-       </Link>
-      </div>
-      <h2 className=" text-base sm:text-xl py-4 px-2  font-semibold ">Popular</h2>
 
-      <div className=" overflow-auto whitespace-nowrap ">
-          {popular.results?.map((item) => (
-            
-            <InlineCard key={item.id} movie={item} />
-            
-          ))}
+        <div className=" px-4 md:px-10 py-2 absolute max-w-[330px] md:max-w-[800px] top-20 md:top-32  ">
+          <h1 className=" text-[24px] mb-2  md:text-3xl font-bold">
+            {random.title}
+          </h1>
+          <div className="  font-semibold text-sm flex ">
+            <p className="  text-green-600 mr-3">
+              {" "}
+              {Math.round(random.vote_average * 10)}% votes
+            </p>
+            <p>{random.release_date}</p>
           </div>
-     
-    </div>
-    <Trending/>
-    <Upcoming/>
+          <p className=" my-4 line-clamp-4 text-sm text-gray-300 md:text-base">
+            {random.overview}
+          </p>
+          <Link href={`movies/${random.id}`}>
+            <button className="  bg-white flex items-center rounded px-2 py-1 font-bold text-black">
+              <BsLink45Deg className="  " /> Open
+            </button>
+          </Link>
+        </div>
+        <h2 className=" text-base sm:text-xl py-4 px-2  font-semibold ">
+          Popular
+        </h2>
+
+        <div className=" overflow-auto whitespace-nowrap ">
+          {popular.results?.map((item) => {
+            <InlineCard key={item.id} movie={item} />;
+          })}
+        </div>
+      </div>
+      <Trending />
+      <Upcoming />
     </div>
   );
-};
+}
--- app/(components)/Modal.jsx
+++ app/(components)/Modal.jsx
@@ -1,14 +1,32 @@
 "use client";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
-import { Fragment, useState } from "react";
-import { useRouter } from 'next/navigation'
-import {AiOutlineClose} from "react-icons/ai"
+import React, { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { AiOutlineClose } from "react-icons/ai";
+import ReactPlayer from "react-player/lazy";
 
 export default function MyModal({ movie }) {
-  const router = useRouter()
+  const router = useRouter();
 
-  let [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+  const [trailer, setTrailer] = useState("");
+
+  React.useEffect(() => {
+    const getShow = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${process.env.NEXT_PUBLIC_API}&language=en-US&append_to_response=videos`
+      );
+      const data = await response.json();
+      if (data?.videos) {
+        const trailerIndex = data.videos.results.findIndex(
+          (item) => item.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[trailerIndex]?.key ?? "");
+      }
+    };
+    getShow();
+  }, []);
 
   function closeModal() {
     setIsOpen(true);
@@ -29,10 +47,12 @@ export default function MyModal({ movie }) {
       </div>
 
       <Transition appear show={isOpen} as={Fragment}>
-  
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-        <div className="fixed inset-0   backdrop-blur  bg-white/10  " aria-hidden="true" />
-  
+          <div
+            className="fixed inset-0   backdrop-blur  bg-white/10  "
+            aria-hidden="true"
+          />
+
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -56,60 +76,70 @@ export default function MyModal({ movie }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                
                 <Dialog.Panel className=" w-full md:max-w-3xl relative transform overflow-hidden  rounded-lg bg-neutral-900 text-left align-middle shadow-xl transition-all">
-                  
-                <button type="button" className=" absolute right-2 top-2 z-30"  onClick={() => router.back()}>
-      <AiOutlineClose className=" text-4xl rounded-full p-2 hover:bg-neutral-900 bg-neutral-900/50 "/>
-    </button>
-  <div className="  w-full  relative    aspect-video  ">
+                  <button
+                    type="button"
+                    className=" absolute right-2 top-2 z-30"
+                    onClick={() => router.back()}
+                  >
+                    <AiOutlineClose className=" text-4xl rounded-full p-2 hover:bg-neutral-900 bg-neutral-900/50 " />
+                  </button>
+                  <div className="  w-full  relative    aspect-video  ">
+                    <ReactPlayer
+                      width="100%"
+                      height="100%"
+                      muted={true}
+                      controls={true}
+                      // url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+                      url={`https://www.youtube.com/watch?v=${trailer}`}
+                    />
 
-
-
-<Image
+                    {/* <Image
   src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
   fill
   placeholder='blur'
   blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8sv5KPQAHjgLYUdlBYAAAAABJRU5ErkJggg=="
   alt=""
   className="  "
-  />
-  </div>
-
-     <div className=" px-3 pb-3 md:px-6 md:pb-6 z-20 pt-3   " >
-                  <Dialog.Title
-                    as="h3"
-                    className=" text-xl md:text-3xl  font-semibold   leading-6 text-white"
-                  >
-                    {movie.title}
-                  </Dialog.Title>
-                  <Dialog.Description>
-                    <div className=" gap-3  my-3 items-end font-semibold text-sm flex ">
-                      <p className="  text-green-600 ">
-                        {Math.round( movie.vote_average*10)}% votes
-                      </p>
-                      <p>{movie.release_date}</p>
-                      <p  className=" border-[1px] text-center  w-8 border-neutral-400   text-neutral-400  text-xs p-1 uppercase">{movie.original_language}</p>
-                    </div>
-                  </Dialog.Description>
-
-
-                  <div className="mt-2">
-                    <p className="line-clamp-3 text-xs text-slate-700 dark:text-slate-50 sm:text-sm">{movie.overview}</p>
+  /> */}
                   </div>
 
+                  <div className=" px-3 pb-3 md:px-6 md:pb-6 z-20 pt-3   ">
+                    <Dialog.Title
+                      as="h3"
+                      className=" text-xl md:text-3xl  font-semibold   leading-6 text-white"
+                    >
+                      {movie.title}
+                    </Dialog.Title>
+                    <Dialog.Description>
+                      <div className=" gap-3  my-3 items-end font-semibold text-sm flex ">
+                        <p className="  text-green-600 ">
+                          {Math.round(movie.vote_average * 10)}% votes
+                        </p>
+                        <p>{movie.release_date}</p>
+                        <p className=" border-[1px] text-center  w-8 border-neutral-400   text-neutral-400  text-xs p-1 uppercase">
+                          {movie.original_language}
+                        </p>
+                      </div>
+                    </Dialog.Description>
 
-                  <div className=" flex   items-baseline mt-4">
+                    <div className="mt-2">
+                      <p className="line-clamp-3 text-xs text-slate-700 dark:text-slate-50 sm:text-sm">
+                        {movie.overview}
+                      </p>
+                    </div>
 
-   <p className=" font-semibold text-base  text-neutral-400  ">Genre:</p>
-              {
-    movie.genres?.map((item)=>(
-      <p className=' ml-2 text-xs ' key={item.id}>{item.name}</p>
-    ))
-  }        
-
-             </div>
-             </div>
+                    <div className=" flex   items-baseline mt-4">
+                      <p className=" font-semibold text-base  text-neutral-400  ">
+                        Genre:
+                      </p>
+                      {movie.genres?.map((item) => (
+                        <p className=" ml-2 text-xs " key={item.id}>
+                          {item.name}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
